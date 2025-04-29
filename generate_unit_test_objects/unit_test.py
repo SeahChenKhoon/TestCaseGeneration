ee7a59dcf00832b913ce680df1356e3f8a99797d
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-04-29 17:04:52 +0800

    feat: Add utility classes for environment variables, LLM prompt execution, and source code representation

---
+++ generate_unit_test_objects/unit_test.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+IMPORT_MAP = {
+    "pytest": "import pytest",
+    "patch": "from unittest.mock import patch",
+    "mock_open": "from unittest.mock import mock_open",
+    "mock": "from unittest import mock",
+    "AsyncMock": "from unittest.mock import AsyncMock"
+}
+
+class SourceCodeFile:
+    def __init__(self, source_code_path, source_code, python_version, requirements_txt):
+        self.source_code = source_code
+        self.source_code_path = source_code_path
+        self.python_version = python_version
+        self.requirements_txt = requirements_txt
+
+class UnitTestComponent:
+    def __init__(self, import_statement, pytest_fixtures, unit_test):
+        self.import_statement = import_statement
+        self.pytest_fixtures = pytest_fixtures
+        self.unit_test = unit_test
+        self.full_unit_test = self.import_statement + "\n\n" + self.pytest_fixtures + "\n\n" + self.unit_test
+
+    def ensure_import_statements(self) -> None:
+        for keyword, import_line in IMPORT_MAP.items():
+            if keyword in self.unit_test and import_line not in self.import_statement:
+                self.import_statement += f"\n{import_line}"
+        self.full_unit_test = self.import_statement + "\n\n" + self.pytest_fixtures + "\n\n" + self.unit_test
+    
+    def update_unit_test(self, unit_test) -> None:
+        self.unit_test = unit_test
+        self.full_unit_test = self.import_statement + "\n\n" + self.pytest_fixtures + "\n\n" + self.unit_test
