d20000428516ed084fb2e515eb2167157555f69e
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-05-03 04:40:49 +0800

    refactor: update cls_SourceCode to use common_helpers and generate module_path attribute

--- TestPilot/source_code.py
+++ TestPilot/source_code.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from TestPilot.logger import setup_logger 
-from TestPilot.settings import cls_Settings
+# from TestPilot.logger import setup_logger
+from TestPilot.common_helpers import setup_logger, cls_Settings
+# from TestPilot.settings import cls_Settings
 
 logger = setup_logger()
 from pathlib import Path
@@ -11,6 +12,7 @@ class cls_SourceCode:
         self.source_code_file_path = source_code_file_path
         self.python_version = settings.python_version
         self.requirements_txt = settings.requirements_txt
+        self.module_path=".".join(Path(self.source_code_file_path).with_suffix("").parts)
 
     def process_source_file(self) -> None:
         logger.info(f"process_source_file starts")
