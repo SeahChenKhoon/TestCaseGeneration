9593ff34ae2575b91e594a8dc93e011cf86da25a
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-05-05 13:50:17 +0800

    feat: integrate cls_TestResult_Set and reporting with test case execution flow and result tracking

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import shutil
-import sys
+import pandas as pd
 
 from typing import NoReturn, List
 from pathlib import Path
+from tabulate import tabulate
 
 from TestPilot.logger import setup_logger 
 from TestPilot.common_helpers import cls_Settings
 from TestPilot.source_code import cls_SourceCode
 from TestPilot.test_cases import cls_Test_Cases
+from TestPilot.test_result import cls_TestResult, cls_TestResult_Set
 
 logger = setup_logger()
 
@@ -47,12 +49,10 @@ def _get_python_files(directory: str) -> List[Path]:
 
 
 def run_initial_setup():
-    logger.info(f"run_initial_setup start")
     # Read Settings
     cls_settings = cls_Settings()
     # Read Housekeep Prcocessing Folders
     clean_test_environment(cls_settings)
-    logger.info(f"run_initial_setup end")
     return cls_settings
 
 from TestPilot.source_code import cls_SourceCode
@@ -60,18 +60,37 @@ from TestPilot.source_code import cls_SourceCode
 def main() -> NoReturn:
     cls_settings = run_initial_setup() 
 
-    test_stats = []
+    
     logger.info(cls_settings.source_dir_str)
     source_dir_list = [path.strip() for path in cls_settings.source_dir_str.split(",") if path]
 
-    cls_test_cases = cls_Test_Cases()    
+    cls_test_cases = cls_Test_Cases()
     for source_dir in source_dir_list:
         source_code_dir = _get_python_files(source_dir)
+        passed_count=0
         for source_code_file in source_code_dir:
             cls_sourcecode = cls_SourceCode(source_code_file, cls_settings)
-            cls_sourcecode.process_source_file()
             cls_test_cases.process_test_cases(cls_sourcecode, cls_settings)
-    logger.info(f"Produce Report")
+            test_stats=[]
+            for idx, test_case in enumerate(cls_test_cases.unit_test, start=1):
+                full_test_case = cls_test_cases.import_statement + "\n\n" + cls_test_cases.pytest_fixtures + "\n\n" + test_case
+                cls_test_result = cls_TestResult(idx, full_test_case)
+                cls_test_result_set = cls_TestResult_Set().process_test_result(cls_test_result, cls_settings, cls_sourcecode)
+                if cls_test_result_set.results[-1].is_passed:
+                    passed_count+=1
+
+            test_stats.append({
+                "filename": source_code_file,
+                "total_test_cases_passed": passed_count,
+                "total_test_cases": idx,
+                "percentage_passed (%)": (passed_count / idx * 100) if idx > 0 else 0.0,
+                "remarks": cls_test_cases.remarks
+            })            
+
+            test_stats_df = pd.DataFrame(test_stats)
+            test_stats_df.index = test_stats_df.index + 1
+            logger.info("\n" + tabulate(test_stats_df, headers='keys', tablefmt='grid'))
+
 
 if __name__ == "__main__":
     main()
