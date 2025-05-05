d49b08a90738c73cb9dc50e260ecc5f054cd7d2f
Seah Chen Khoon <chenkhoon@yahoo.com>
2025-05-05 18:16:10 +0800

    refactor: Integrate SaveFile handling and streamline test result processing and reporting

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from pathlib import Path
 from tabulate import tabulate
 
 from TestPilot.logger import setup_logger 
-from TestPilot.common_helpers import cls_Settings
+from TestPilot.common_helpers import cls_Settings, SaveFile
 from TestPilot.source_code import cls_SourceCode
 from TestPilot.test_cases import cls_Test_Cases
-from TestPilot.test_result import cls_TestResult, cls_TestResult_Set
+from TestPilot.test_result import cls_TestResult
 
 logger = setup_logger()
 
@@ -55,11 +55,11 @@ def run_initial_setup():
     clean_test_environment(cls_settings)
     return cls_settings
 
-from TestPilot.source_code import cls_SourceCode
+
 
 def main() -> NoReturn:
     cls_settings = run_initial_setup() 
-
+    
     
     logger.info(cls_settings.source_dir_str)
     source_dir_list = [path.strip() for path in cls_settings.source_dir_str.split(",") if path]
@@ -68,16 +68,27 @@ def main() -> NoReturn:
     for source_dir in source_dir_list:
         source_code_dir = _get_python_files(source_dir)
         passed_count=0
+        test_stats=[]
+        
         for source_code_file in source_code_dir:
             cls_sourcecode = cls_SourceCode(source_code_file, cls_settings)
+            savefile = SaveFile(source_dir, cls_sourcecode.source_code_file_path)
             cls_test_cases.process_test_cases(cls_sourcecode, cls_settings)
-            test_stats=[]
+            success_unit_test=cls_test_cases.import_statement + "\n\n" + \
+                cls_test_cases.pytest_fixtures
+            idx=0
+            overall_error_msg=""
             for idx, test_case in enumerate(cls_test_cases.unit_test, start=1):
-                full_test_case = cls_test_cases.import_statement + "\n\n" + cls_test_cases.pytest_fixtures + "\n\n" + test_case
-                cls_test_result = cls_TestResult(idx, full_test_case)
-                cls_test_result_set = cls_TestResult_Set().process_test_result(cls_test_result, cls_settings, cls_sourcecode)
-                if cls_test_result_set.results[-1].is_passed:
+                full_test_case = cls_test_cases.import_statement + "\n\n" + \
+                    cls_test_cases.pytest_fixtures + "\n\n" + test_case
+                cls_test_result_list, error_msg_unit_case = cls_TestResult(idx, full_test_case).\
+                    process_test_result(cls_test_cases, cls_settings, cls_sourcecode)
+                last_test_result:cls_TestResult=cls_test_result_list[-1]
+                if last_test_result.is_passed:
                     passed_count+=1
+                    success_unit_test+=cls_test_cases.unit_test[idx-1] + "\n\n"
+                else:
+                    overall_error_msg+=error_msg_unit_case
 
             test_stats.append({
                 "filename": source_code_file,
@@ -85,11 +96,18 @@ def main() -> NoReturn:
                 "total_test_cases": idx,
                 "percentage_passed (%)": (passed_count / idx * 100) if idx > 0 else 0.0,
                 "remarks": cls_test_cases.remarks
-            })            
-
-            test_stats_df = pd.DataFrame(test_stats)
-            test_stats_df.index = test_stats_df.index + 1
-            logger.info("\n" + tabulate(test_stats_df, headers='keys', tablefmt='grid'))
+            })
+            if passed_count > 0:
+                savefile.save_file(Path(cls_settings.finalized_tests_dir), success_unit_test)
+                is_passed, err_msg=cls_TestResult(1,success_unit_test).run_unit_test(cls_settings, cls_sourcecode)
+                if not is_passed:
+                    logger.error(f"Error when compiling code - {err_msg}")
+                            
+            if overall_error_msg:
+                savefile.save_file(Path(cls_settings.failed_tests_dir), overall_error_msg, prefix="err_", file_extension=".log")
+        test_stats_df = pd.DataFrame(test_stats)
+        test_stats_df.index = test_stats_df.index + 1
+        logger.info("\n" + tabulate(test_stats_df, headers='keys', tablefmt='grid'))
 
 
 if __name__ == "__main__":
